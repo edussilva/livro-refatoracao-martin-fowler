9b79e6626dac2700a6f42409093fbe4c119f1749
edussilva <edussilva@live.com>
2023-06-03 05:40:11 -0300

    added test for cap1

---
+++ cap1/test_cap1.py
@@ -0,0 +1,37 @@
+import pytest
+from cap1 import statement
+
+def test_success_statement_function():
+    plays = {
+        "hamlet": {"name": "Hamlet", "type": "tragedy"},
+        "as-like": {"name": "As You Like It", "type": "comedy"},
+        "othello": {"name": "Othello", "type": "tragedy"}
+    }
+    invoice = {
+        "customer": "BigCo",
+        "performances": [
+            {
+                "playID": "hamlet",
+                "audience": 55
+            },
+            {
+                "playID": "as-like",
+                "audience": 35
+            },
+            {
+                "playID": "othello",
+                "audience": 40
+            }
+        ]
+    }
+    
+    result = (
+        'Statement for BigCo\n'
+        ' Hamlet: 650.00 (55 seats)\n'
+        ' As You Like It: 580.00 (35 seats)\n'
+        ' Othello: 500.00 (40 seats)\n'
+        'Amount owed is 1,730.00\n'
+        'You earned 47 credits\n'
+    )
+
+    assert statement(invoice, plays) == result
