d8851d3988a98810419c8a29206ac97bbc0ec893
edussilva <edussilva@live.com>
2023-06-13 17:49:54 -0300

    Refactor files

--- cap1/tests/test_statement.py
+++ cap1/tests/test_statement.py
@@ -1,5 +1,5 @@
-import pytest
-from statement import statement, html_statement
+# import pytest
+from cap1.statement import statement
 
 
 # def test_ok_amount_for_play_tragedy():
@@ -69,42 +69,3 @@ def test_success_statement_function():
     )
 
     assert statement(invoice, plays) == result
-
-
-def test_success_html_statement_function():
-    plays = {
-        "hamlet": {"name": "Hamlet", "type": "tragedy"},
-        "as-like": {"name": "As You Like It", "type": "comedy"},
-        "othello": {"name": "Othello", "type": "tragedy"}
-    }
-    invoice = {
-        "customer": "BigCo",
-        "performances": [
-            {
-                "playID": "hamlet",
-                "audience": 55
-            },
-            {
-                "playID": "as-like",
-                "audience": 35
-            },
-            {
-                "playID": "othello",
-                "audience": 40
-            }
-        ]
-    }
-    
-    result = (
-        '<h1>Statement for BigCo</h1>\n'
-        '<table>\n'
-        '  <tr><th>play</th><th>seats</th><th>cost</th></tr>\n'
-        '  <tr><td>Hamlet</td><td>55</td><td>650.00</td></tr>\n'
-        '  <tr><td>As You Like It</td><td>35</td><td>580.00</td></tr>\n'
-        '  <tr><td>Othello</td><td>40</td><td>500.00</td></tr>\n'
-        '</table>\n'
-        '<p>Amount owed is <em>1,730.00</em></p>\n'
-        '<p>You earned <em>47</em> credits</p>\n'
-    )
-
-    assert html_statement(invoice, plays) == result
